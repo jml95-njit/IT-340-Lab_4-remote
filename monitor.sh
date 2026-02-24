69969a151b665947b016c9b665d85c5cf903f3cf
Jeremy Labarre <jml95@njit.edu>
2026-02-23 20:06:15 -0500

    Updated to SYSTEM REPORT on disk-space branch

--- monitor.sh
+++ monitor.sh
@@ -1,3 +1,3 @@
-echo "Memory Log - $(date)" >> system_log.txt
+echo "SYSTEM REPORT (Memory) - $(date)" >> system_log.txt
 free -h | grep Mem >> system_log.txt
 echo "--------------------------------" >> system_log.txt
